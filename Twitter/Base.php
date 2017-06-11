e95bfb8959d444d1c57ee96e1dc32c5085c613e7
Joseph Mtinangi <josephmtinangi@gmail.com>
2017-06-11 14:04:31 +0300

    Initialize protected variables

--- Twitter/Base.php
+++ Twitter/Base.php
@@ -5,6 +5,10 @@ namespace Twitter;
 class Base
 {
     const API_URL = "https://api.twitter.com/1.1";
-    
+
+    protected $client;
+    protected $token;
+    protected $tokenSecret;
+    protected $accessToken;
 
 }
