e92e47f0e0c68a39374d20434a5959e32088ea89
Joseph Mtinangi <josephmtinangi@gmail.com>
2017-06-11 14:06:25 +0300

    Initialize guzzle client in constructor

--- Twitter/Base.php
+++ Twitter/Base.php
@@ -2,6 +2,8 @@
 
 namespace Twitter;
 
+use GuzzleHttp\Client;
+
 class Base
 {
     const API_URL = "https://api.twitter.com/1.1";
@@ -10,5 +12,13 @@ class Base
     protected $token;
     protected $tokenSecret;
     protected $accessToken;
+
+    /**
+     * Base constructor.
+     */
+    public function __construct()
+    {
+        $this->client = new Client();
+    }
 
 }
